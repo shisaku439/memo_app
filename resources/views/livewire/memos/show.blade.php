dd799f68d28cd57ffb7c78e004f3a41ce2c336e7
shisaku439 <shisaku555@gmail.com>
2025-10-11 15:49:21 +0900

    戻るリンクを追加

--- resources/views/livewire/memos/show.blade.php
+++ resources/views/livewire/memos/show.blade.php
@@ -8,6 +8,7 @@ state(['memo' => fn(Memo $memo) => $memo]);
 ?>
 
 <div>
+    <a href="{{ route('memos.index') }}">戻る</a>
     <h1>{{ $memo->title }}</h1>
     {{-- laravelでは、{{}}で自動的にエスケープされる --}}
     {{-- {{!! !!}}でエスケープを無効化 --}}
