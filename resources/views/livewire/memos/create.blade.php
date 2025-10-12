e618aa71f732643877cd12f3d8cf134510330364
shisaku439 <shisaku555@gmail.com>
2025-10-12 09:33:33 +0900

    バリデーション実装

--- resources/views/livewire/memos/create.blade.php
+++ resources/views/livewire/memos/create.blade.php
@@ -1,12 +1,18 @@
 <?php
 
-use function Livewire\Volt\{state};
+use function Livewire\Volt\{state, rules};
 use App\Models\Memo;
 
 state(['title', 'body']);
 
+rules([
+    'title' => 'required|string|max:50',
+    'body' => 'required|string|max:2000',
+]);
+
 // メモを保存する関数
 $store = function () {
+    $this->validate(); //バリデーションチェック
     // フォームからの入力値をデータベースへ保存
     // Memo::create([
     //     'title' => $this->title,
@@ -26,12 +32,20 @@ $store = function () {
     <!-- wire:submit="store"でフォーム送信時にstore関数を呼び出し -->
     <form wire:submit="store">
         <p>
-            <label for="title">タイトル</label><br>
+            <label for="title">タイトル</label>
+            @error('title')
+                <span class='error'>({{ $message }})</span>
+            @enderror
+            <br>
             <!-- wire:model="title"で入力値とコンポーネントの状態($this->title)を自動的に同期 -->
             <input type="text" wire:model="title" id="title">
         </p>
         <p>
-            <label for="body">本文</label><br>
+            <label for="body">本文</label>
+            @error('body')
+                <span class="error">({{ $message }})</span>
+            @enderror
+            <br>
             <!-- wire:model="body"で入力値とコンポーネントの状態($this->body)を自動的に同期 -->
             <textarea wire:model="body" id="body"></textarea>
         </p>
--- resources/views/livewire/memos/edit.blade.php
+++ resources/views/livewire/memos/edit.blade.php
@@ -1,10 +1,15 @@
 <?php
 
-use function Livewire\Volt\{state, mount};
+use function Livewire\Volt\{state, mount, rules};
 use App\Models\Memo;
 
 //フォームの状態を管理
 state(['memo', 'title', 'body']);
+
+rules([
+    'title' => 'required|string|max:50',
+    'body' => 'required|string|max:2000',
+]);
 
 //ルートモデルバインディングはmountでまとめて行う
 mount(function (Memo $memo) {
@@ -14,6 +19,8 @@ mount(function (Memo $memo) {
 });
 
 $update = function () {
+    $this->validate(); //バリデーションチェック
+
     $this->memo->update($this->all());
     return redirect()->route('memos.show', $this->memo);
 };
@@ -26,12 +33,20 @@ $update = function () {
     <!-- wire:submit="update"でフォーム送信時にupdate関数を呼び出し -->
     <form wire:submit="update">
         <p>
-            <label for="title">タイトル</label><br>
+            <label for="title">タイトル</label>
+            @error('title')
+                <span class="error">({{ $message }})</span>
+            @enderror
+            <br>
             <!-- wire:model="title"で入力値とコンポーネントの状態($this->title)を自動的に同期 -->
             <input type="text" wire:model="title" id="title">
         </p>
         <p>
-            <label for="body">本文</label><br>
+            <label for="body">本文</label>
+            @error('body')
+                <span class="error">({{ $message }})</span>
+            @enderror
+            <br>
             <!-- wire:model="body"で入力値とコンポーネントの状態($this->body)を自動的に同期 -->
             <textarea wire:model="body" id="body"></textarea>
         </p>
