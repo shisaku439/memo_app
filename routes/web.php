7863fcef9bdf07197752c11518bd6a675d4839cf
shisaku439 <shisaku555@gmail.com>
2025-10-25 09:32:09 +0900

    Renderデプロイのための設定

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use Livewire\Volt\Volt;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Volt::route('/', 'memos.index')->name('memos.index');
 
 // 一覧ページ
 Volt::route('/memos', 'memos.index')->name('memos.index');
